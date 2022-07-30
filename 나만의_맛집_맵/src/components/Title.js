b86d183d3b46e4fcdcb321a52f34b844f16b8c2f
Gotkwondo <dhwngus99@gmail.com>
2022-07-30 19:20:43 +0900

    레이아웃(5)

--- 나만의_맛집_맵/src/components/Title.js
+++ 나만의_맛집_맵/src/components/Title.js
@@ -1,14 +1,24 @@
 import '../styles/title.scss';
 import GitImage from "../static/img/kisspng-github-computer-software-logo-source-code-github-logo-png-5cc8c808c04229.7905269215566622807875.png";
+import Tistory from "../static/img/tistory.svg";
 
 const Title = () => {
   return (
     <div className="hd-wrapper">
       <div className="title">InMate</div>
       <div className="img-wrapper">
-        <a href="https://github.com/Gotkwondo/Side-Project/tree/main/%EB%82%98%EB%A7%8C%EC%9D%98_%EB%A7%9B%EC%A7%91_%EB%A7%B5">
-          <img src={GitImage} />
-        </a>
+        <ul>
+          <li>
+            <a href="https://github.com/Gotkwondo/Side-Project/tree/main/%EB%82%98%EB%A7%8C%EC%9D%98_%EB%A7%9B%EC%A7%91_%EB%A7%B5">
+              <img src={GitImage} />
+            </a>
+          </li>
+          <li>
+            <a href="https://58cjdcns99.tistory.com/">
+              <img src={Tistory} />
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   )

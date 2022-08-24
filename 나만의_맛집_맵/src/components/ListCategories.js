444737bd9f75e9f2a13a39b2e9717db036f05c1e
Gotkwondo <dhwngus99@gmail.com>
2022-08-24 19:58:16 +0900

    리스트 스크롤 & 컴포넌트 최적화

--- 나만의_맛집_맵/src/components/ListCategories.js
+++ 나만의_맛집_맵/src/components/ListCategories.js
@@ -1,26 +1,62 @@
-const ListItem = ({ info, onSelectLocation }) => {
+import { List, AutoSizer } from 'react-virtualized';
+import { useCallback } from 'react';
+
+const ListItem = ({ info, onSelectLocation, style }) => {
   return (
     //  onClick을 사용할 때는 항상 함수 타입이 주어져야 된다.
-    <li onClick={() => onSelectLocation(info.id)}>
-      <span className='name'>{info.name}</span>
-      <br />
-      <span className='kind'>{info.kind}</span>
+    <li onClick={() => onSelectLocation(info.id)} style={style}>
+      <em className='name'>{info.name}</em>
+      {/* <br /> */}
+      <p className='kind'>{info.adress}</p>
+      <p>{info.phone}</p>
     </li>
   );
 };
 
 
 const ListCategories = ({ onSelectLocation, infos }) => {
-  return (
-    <ul>
-      {infos.map(info => (
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const info = infos[index];
+      return (
         <ListItem
           onSelectLocation={onSelectLocation}
           info={info}
-          key={info.id}
+          key={key}
+          style={style}
         />
-      ))}
-    </ul>
+      )
+    },[onSelectLocation, infos]
+  )
+
+
+  return (
+    <div className="left-innerWrapper" style={{width:"100%", height:"80vh"}}>
+      <AutoSizer>
+        {({ width, height }) => (
+          <List
+            className="list"
+            width={width}   //  전체 크기
+            height={height}  //  전체 높이
+            rowCount={infos.length} //  항목 개수
+            rowHeight={150}  //  항목 높이
+            rowRenderer={rowRenderer} //  항목을 렌더링할 때 쓰는 함수
+            list={infos}  //  배열
+            style={{ outline: 'none' }} //  List에 기본 적용되는 outline 스타일 제거
+          ></List>
+        )}
+        
+      </AutoSizer>
+      {/* <ul>
+        {infos.map(info => (
+          <ListItem
+            onSelectLocation={onSelectLocation}
+            info={info}
+            key={info.id}
+          />
+        ))}
+      </ul> */}
+    </div>
   );
 };
 

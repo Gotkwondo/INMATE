ba4793cd33512eb68d20c4f13caecd73fed228c0
Gotkwondo <dhwngus99@gmail.com>
2022-08-30 17:52:10 +0900

    스타일링(4)

--- 나만의_맛집_맵/src/components/Bar.js
+++ 나만의_맛집_맵/src/components/Bar.js
@@ -6,12 +6,14 @@ const Bar = () => {
       <div className="bar_contents">
         <div className="text">
           <p>Designed by Oh Ju Hyeon</p>
+          <br/>
           <p>E-Mail : dhwngus99@gmail.com</p>
         </div>
         <div className="link">
           <p>
             코드 링크 : <a href="https://github.com/Gotkwondo/Side-Project/tree/main/%EB%82%98%EB%A7%8C%EC%9D%98_%EB%A7%9B%EC%A7%91_%EB%A7%B5">https://github.com/Gotkwondo/Side-Project/tree/main/나만의_맛집_맵</a>
           </p>
+          <br/>
           <p>
             블로그 : <a href="https://58cjdcns99.tistory.com/">https://58cjdcns99.tistory.com/</a>
           </p>

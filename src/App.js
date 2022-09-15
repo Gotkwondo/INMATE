281dadf909b5ea9a1655fb39c0fed530cefad544
Gotkwondo <dhwngus99@gmail.com>
2022-09-15 14:37:23 +0900

    주석 제거

--- src/App.js
+++ src/App.js
@@ -4,14 +4,7 @@ import Bar from './components/modules/Bar/Bar';
 
 
 const App = () => {
-  // const Desktop = useMediaQuery({
-  //   query: "(min-width: 1024px)"
-  // })
-  // const Mobile = useMediaQuery({
-  //   query: "(max-width: 1023px)"
-  // })
   return (
-    
     <div className="wrapper">
       <div className="container">
         <Title />

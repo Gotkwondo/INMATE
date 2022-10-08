b1d011031f1ab347589382843d8094b7f34cc2bb
Gotkwondo <dhwngus99@gmail.com>
2022-10-08 18:14:30 +0900

    Info3 스타일 작업

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,15 +1,15 @@
 import GifAnimation from '../modules/GifAnimation';
-import testImage from '../../static/img/test_Image.jpg';
 import mapGif from './../../static/gif/mapGif.gif';
 import mapStaticImg from '../../static/img/mapStaticImg.gif'
+import infoGif from '../../static/gif/infoGif.gif'
+import infoStatic from '../../static/img/infoStatic.jpg'
 
 const Main = () => {
   return (
     <div className="main_contents">
       <div className="main_contents_list">
         <GifAnimation url="/INMATE/maplist" img={mapStaticImg} text="인천 맛집 지도" gif={mapGif}/>
-        <GifAnimation url="/INMATE/info" img={testImage} text="웹의 정보" gif={mapGif }/>
-        <GifAnimation url="/INMATE/dev" img={testImage} text="개발일지" gif={ mapGif}/>
+        <GifAnimation url="/INMATE/info" img={infoStatic} text="웹의 정보" gif={infoGif }/>
       </div>
     </div>
   );
